26274861aa1722bc5f49292442fbba8395a21aec
Danil <Dpopov25@yandex.ru>
2023-01-15 16:06:29 +0500

    ref

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import Header from "../../components/Header/Header";
 import classes from "../General/General.module.css";
 import Footer from "../../components/Footer/Footer";
+import {UserStatus} from "../../UserStatus";
 
 const Profile = () => {
+    const {authStatus, userId} = useContext(UserStatus)
+
+    useEffect(() => {
+        if (authStatus === "auth") {
+            fetch("/api/profile/" + userId, {
+                method: "GET",
+                cache: "no-cache",
+                headers: {
+                    "content-type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(response => {
+                    console.log(response)
+                });
+        };
+    });
+
     return (
         <div className="profile">
             <Header></Header>
--- server/app/routes/user/profile.py
+++ server/app/routes/user/profile.py
@@ -7,22 +7,21 @@ from bson import ObjectId
 profile_router = Blueprint('Profile', __name__, url_prefix="/api")
 
 # Регистрация
-@profile_router.route('/profile/<id>', methods=["POST","GET"])
+@profile_router.get('/profile/<id>',)
 def profile(id):
-    if request.method == 'GET':
-        db = db_connect()
-        collections = db.Users
-        collections_purchased = db.Purchased
+    db = db_connect()
+    collections = db.Users
+    collections_purchased = db.Purchased
 
-        user_data = db_find(collections, {"_id": ObjectId(id)})
-        new_user_data = UserDto(user_data).get_dict()
+    user_data = db_find(collections, {"_id": ObjectId(id)})
+    new_user_data = UserDto(user_data).get_dict()
 
-        purchased_game = db_find(collections_purchased, {"user_id": id})
-        new_purchased_game = PurchasedDto(purchased_game).get_dict()
+    purchased_game = db_find(collections_purchased, {"user_id": id})
+    new_purchased_game = PurchasedDto(purchased_game).get_dict()
 
-        data = {
-            "user_data": new_user_data,
-            "purchased_game": new_purchased_game
-        }
+    data = {
+        "user_data": new_user_data,
+        "purchased_game": new_purchased_game
+    }
 
-        return jsonify(data)
+    return jsonify(data)
